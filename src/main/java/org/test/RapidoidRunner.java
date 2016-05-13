7c725bae7290e38ffdd6303691fc92d1c8bacb3c
sagnitude <leyicong@gmail.com>
2016-05-13 22:27:59 +0800

    added a simple test

---
+++ src/main/java/org/test/RapidoidRunner.java
@@ -0,0 +1,20 @@
+package org.test;
+
+import org.rapidoid.http.Req;
+import org.rapidoid.setup.On;
+import org.rapidoid.web.Rapidoid;
+
+/**
+ * Created by sagnitude on 5/13/2016.
+ */
+public class RapidoidRunner {
+
+    public static void main(String[] args) {
+
+        On.address("0.0.0.0").port(5432);
+
+        Rapidoid.run(args);
+
+        On.get("/showUri").json(Req::uri);
+    }
+}
